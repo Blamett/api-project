7c78937020edbf71ecdbc6b25bdef0e6c172d88c
Artur Baldo Ramos <Artur Baldo Ramos>
2023-05-27 15:37:09 -0300

    Changed jwt expire time and added some comments

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { PrismaService } from 'src/prisma.service';
         JwtModule.register({
             global: true,
             secret: process.env.JWT_SECRET,
-            signOptions: { expiresIn: '60s' },
+            signOptions: { expiresIn: '2d' },
           }),
     ],
     controllers: [
